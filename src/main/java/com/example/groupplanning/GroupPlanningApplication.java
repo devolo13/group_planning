a9efb4df034a63af2909f64735249029ff4b812e
devolo13 <devolo13@gmail.com>
2023-06-26 12:59:34 -0500

    initial commit

---
+++ src/main/java/com/example/groupplanning/GroupPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.example.groupplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupPlanningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/groupplanning/GroupPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groupplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPlanningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
